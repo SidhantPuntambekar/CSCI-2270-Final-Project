8ae8b58126e8c91b69172c0f09e6aaf446ed89ed
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-04-17 07:49:49 -0600

    Added driver file for linked list

---
+++ linkedList/driver.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <limits>
+#include <fstream>
+#include <sstream>
+#include "LinkedList.h"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int testData[40000];
+    float insert[400];
+    float search[400];
+    string openFile = argv[1];
+    ifstream LinkedListTestData(openFile);
+    if (LinkedListTestData.fail())
+    {
+        return -1;
+    }
+    else
+    {
+        string line;
+        int number = 0; 
+        int idx = 0; 
+        while (getline(LinkedListTestData, line))
+        {
+            stringstream ss(line);
+            while (getline(ss, number, ','))
+            {
+                testData[idx] = number;
+            }
+        }
+    }
+}
